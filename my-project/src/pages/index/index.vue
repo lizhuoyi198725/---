7d37693545116223263c30dc19d267ac6018916f
mazhenzhen <mario1201@foxmail.com>
2018-09-20 13:15:40 +0800

    shehuipin

---
+++ my-project/src/pages/index/index.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <div class="container">
+    <div class="topNav">
+
+    </div>
+    <card></card>
+    <div class="bottomNav">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello 社惠拼',
+      userInfo: {}
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
